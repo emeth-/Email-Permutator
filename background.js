fe75c5aa167b9a5985c621e78df6abf40c05df8b
emeth- <seanybob@gmail.com>
2017-05-29 07:59:37 -0500

    dropped list support, added profile support

--- background.js
+++ background.js
@@ -3,29 +3,43 @@ chrome.browserAction.setIcon({
 });
 
 
-function send_to_tab() {
+function send_to_tab_extension_clicked() {
+    send_to_tab({trigger_extractor: "doit"});
+}
+
+function send_to_tab(data) {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {trigger_extractor: "doit"}, function(response) {
-            console.log("done!");
+        chrome.tabs.sendMessage(tabs[0].id, data, function(response) {
+            console.log("Sent to tab...", data);
         });
     });
 }
 
+chrome.browserAction.onClicked.addListener(send_to_tab_extension_clicked);
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    console.log("sb1", typeof(message.badgetext), message.badgetext);
     if ("badgetext" in message) {
         chrome.browserAction.setBadgeText({text: message.badgetext});
     }
+    else if ("dns_check" in message) {
+        //Gotta do this here, because it's http and linkedin is https
+        $.ajax({
+            url:"http://viewdns.info/reversewhois/?q="+message['dns_check'],
+            type:'GET',
+            current_email: message['dns_check'],
+            success: function(data){
+                var domain_results = data.split('Reverse Whois results for')[1].split('domains that matched this search query')[0].split('There are')[1].trim();
+                console.log("DNS check for", this.current_email, domain_results)
+                //Note, if this api goes down, swap to http://www.whoismind.com/email/myemail@gmail.com.html
+                domain_results = parseInt(domain_results);
+                if (domain_results > 0) {
+                    send_to_tab({found_email_dns: this.current_email});
+                }
+            },
+            cache: false
+        });
+    }
 });
-
-
-chrome.runtime.onInstalled.addListener(function(details) {
-    if (details.reason.search(/install/g) === -1) return;
-    chrome.runtime.openOptionsPage();
-});
-
-
-chrome.browserAction.onClicked.addListener(send_to_tab);
 
 var requestFilter = {
     urls: [ "<all_urls>" ]
@@ -39,6 +53,7 @@ var requestFilter = {
     if (details.url.indexOf('https://mail.google.com/mail/gxlu?email=') !== -1) {
         for( var i = 0, l = headers.length; i < l; ++i ) {
           if( headers[i].name == 'Cookie' ) {
+            //Blank out user's logged in session for this request, as check doesn't work if they are already logged in.
             headers[i].value = '';
             break;
           }
@@ -51,33 +66,20 @@ var requestFilter = {
 
 chrome.webRequest.onBeforeSendHeaders.addListener( handler, requestFilter, extraInfoSpec );
 
-
-
-
-
-
-
-
-
-
 chrome.webRequest.onHeadersReceived.addListener(
     function (details) {
         if (details.url.indexOf('https://mail.google.com/mail/gxlu?email=') !== -1) {
             details.responseHeaders.forEach(function(responseHeader){
                 if (responseHeader.name.toLowerCase() === "set-cookie") {
+                    //Method from here: https://blog.0day.rocks/abusing-gmail-to-get-previously-unlisted-e-mail-addresses-41544b62b2
+                    //Gmail classified it as not a bug.
 
                     var parser = document.createElement('a');
                     //parser.href = "https://mail.google.com/mail/gxlu?email=bobdole@gmail.com&_=1494707114226";
                     parser.href = details.url;
                     email = parser.search.split('email=')[1].split('&')[0]
 
-                    console.log("VICTORY", responseHeader.value)
-
-                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-                        chrome.tabs.sendMessage(tabs[0].id, {found_email: email}, function(response) {
-                            console.log("done!");
-                        });
-                    });
+                    send_to_tab({found_email: email});
                 }
             });
         }
--- js/loader.js
+++ js/loader.js
@@ -1,32 +1,8 @@
 var running = 0;
 var start_time = new Date().getTime();
 var li_extractor_data = [];
-var total_pages = 0;
-var email_address_results = "";
-var max_num_search_pages = 5;
-var liextractor_api_url = "";
-
-chrome.storage.sync.get(null, function(items) {
-
-    if (items['email_address_results']) {
-        email_address_results = items['email_address_results'];
-    }
-
-    if (items['max_num_search_pages']) {
-        max_num_search_pages = items['max_num_search_pages'];
-    }
-
-    if (items['liextractor_api_url']) {
-        liextractor_api_url = items['liextractor_api_url'];
-    }
-});
-
-function round(value, decimals) {
-  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
-}
-
 function get_domain(name, company_name) {
-    console.log("sb2", name, company_name);
+    console.log("get_domain", name, company_name);
     $.ajax({
         type: 'GET',
         url: "https://autocomplete.clearbit.com/v1/companies/suggest?query="+company_name,
@@ -95,6 +71,7 @@ function scan_for_emails(name, domain) {
         emails.push(new_e.toLowerCase());
     }
     console.log("attempted emails...", emails);
+    var ms_delay = 0;
     for(var i=0; i<emails.length; i++) {
         $.ajax({
             type: 'GET',
@@ -120,23 +97,9 @@ function scan_for_emails(name, domain) {
             cache: false
         });
 
-        $.ajax({
-            url:"http://viewdns.info/reversewhois/?q="+emails[i],
-            type:'GET',
-            current_email: emails[i],
-            success: function(data){
-                var domain_results = data.split('Reverse Whois results for')[1].split('domains that matched this search query')[0].split('There are')[1].trim();
-                console.log("DNS check for", this.current_email, domain_results)
-                //Note, if this api goes down, swap to http://www.whoismind.com/email/myemail@gmail.com.html
-                domain_results = parseInt(domain_results);
-                if (domain_results > 0) {
-                    found_email(this.current_email, "DNS");
-                }
-            },
-            cache: false
-        });
+        chrome.runtime.sendMessage({"dns_check": emails[i]});
 
-        $.ajax({
+        ajax_after_milliseconds({
             url:"https://haveibeenpwned.com/api/v2/breachedaccount/"+emails[i],
             type:'GET',
             current_email: emails[i],
@@ -144,274 +107,107 @@ function scan_for_emails(name, domain) {
                 found_email(this.current_email, "HAVEIBEENPWNED");
             },
             cache: false
-        });
-
+        }, ms_delay);
+        ms_delay += 1800;
 
     }
 
 }
 
-function get_profile_info() {
-    var name = $.trim($('.pv-top-card-section__name').text());
-    var company_name = $.trim($('.pv-top-card-section__company').text());
-    console.log("sb1");
-    get_domain(name, company_name);
+function ajax_after_milliseconds(ajaxconfig, timeperiod) {
+    setTimeout(function(){
+        $.ajax(ajaxconfig);
+    }, timeperiod);
+}
+
+function scan_for_profile() {
+    if ($('.pv-top-card-section__name').length > 0 && $('.pv-top-card-section__company').length > 0) {
+        //normal account logged in linkedin profile
+        var name = $.trim($('.pv-top-card-section__name').text());
+        var company_name = $.trim($('.pv-top-card-section__company').text());
+        console.log("get_profile_info", name, company_name);
+        get_domain(name, company_name);
+    }
+    else if ($('.profile-info').find('.member-name').length > 0 && $('.profile-info').find('.title').length > 0) {
+        //Sales navigator linkedin profile
+        var name = $.trim($('.profile-info').find('.member-name').text());
+        var company_name = $('.profile-info').find('.title').text().split(' at ');
+        if (company_name.length > 1) {
+            company_name = $.trim(company_name[1])
+        }
+        else {
+            company_name = "";
+        }
+        console.log("get_profile_info", name, company_name);
+        get_domain(name, company_name);
+    }
+    else if ($('.profile-overview-content').find('#name').length > 0 && $('.profile-overview-content').find('.title').length > 0) {
+        //logged out linkedin profile
+        var name = $.trim($('.profile-overview-content').find('#name').text());
+        var company_name = $('.profile-overview-content').find('.title').text().split(' at ');
+        if (company_name.length > 1) {
+            company_name = $.trim(company_name[1])
+        }
+        else {
+            company_name = "";
+        }
+        console.log("get_profile_info", name, company_name);
+        get_domain(name, company_name);
+    }
+    else {
+        setTimeout(function(){
+            scan_for_profile();
+        }, 300)
+    }
 }
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+    console.log("message received...........", request);
     if (request.trigger_extractor == "doit") {
-        console.log("sb1 TRIGGERED");
-        get_profile_info();
+        scan_for_profile();
     }
     else if (request.found_email) {
         found_email(request.found_email, "gmail");
     }
+    else if (request.found_email_dns) {
+        found_email(request.found_email_dns, "DNS");
+    }
 });
 
 function found_email(email, source) {
-    alert("Found email: "+email+"\nSource: "+source);
+    console.log("****Found Email", email, source);
+
+    var source_text = source;
+    if (source == 'github') {
+        source_text = "<a target='_blank' href='https://api.github.com/search/commits?q=committer-email:"+email+"'>github</a>";
+    }
+    if (source == 'HAVEIBEENPWNED') {
+        source_text = "<a target='_blank' href='https://haveibeenpwned.com/api/v2/breachedaccount/"+email+"'>HIBP</a>";
+    }
+    if (source == 'DNS') {
+        source_text = "<a target='_blank' href='http://viewdns.info/reversewhois/?q="+email+"'>DNS</a>";
+    }
+
+    if ($('.pv-top-card-section__headline').length > 0) {
+        $('.pv-top-card-section__headline').after('<h2 class="Sans-19px-black-85%" title="Source: '+source+'">'+email+' ['+source_text+']</h2>');
+    }
+    else if ($('.profile-overview-content').find('.title').length > 0) {
+        $('.profile-overview-content').find('.title').after('<p class="headline email" data-section="headline" title="Source: '+source+'">'+email+' ['+source_text+']</p>');
+    }
+    else if ($('.liext-emails').length > 0) {
+        var htmlz = '';
+        htmlz += '<ul class="liext-emails"><li>'+email+' ['+source_text+']</li></ul>';
+        $('.liext-emails').append(htmlz);
+    }
+    else if ($('.profile-info').find('.title').length > 0) {
+        var htmlz = '<h4 class="sub-headline">Emails</h4>';
+        htmlz += '<ul class="liext-emails"><li>'+email+' ['+source_text+']</li></ul>';
+        $('.profile-info').find('.title').parent().after(htmlz);
+    }
+
 }
 
-function next_page_loaded() {
-    console.log("Next page loaded check!");
-
-    var is_loading = false;
-    //Sales Navigator
-    if (jQuery(".results-loader-wrapper").find('.results-loader:visible').length > 0) {
-        is_loading = true;
-    }
-    //Normal LinkedIn
-    if (jQuery('.search-is-loading.search-results-container').length > 0) {
-        is_loading = true;
-    }
-
-    if (is_loading) {
-        //Search results loading...
-        setTimeout(function(){
-            next_page_loaded();
-        }, 300);
-    }
-    else {
-        //Search results completed loading!
-        extract_data();
-    }
-}
-
-function next_page() {
-    console.log("triggering next page...", running);
-    if (running && total_pages <= max_num_search_pages) {
-        console.log("Next page triggered!", jQuery(".next-pagination"), jQuery(".next-pagination").length);
-
-        //Normal LinkedIn
-        jQuery(".results-paginator").find(".next").trigger('click');
-
-        //Sales Navigator
-        Array.from(document.getElementsByClassName("next-pagination")).forEach(function(element) {
-            element.click();
-        });
-
-        setTimeout(function(){
-            next_page_loaded();
-        }, 1000);
-    }
-    else {
-        //END
-        var end_time = new Date().getTime();
-        var elapsed_seconds = (end_time-start_time)/1000;
-        var elapsed_minutes = elapsed_seconds/60;
-        elapsed_minutes = round(elapsed_minutes, 1);
-        alert("Completed pulling info from website (took "+elapsed_minutes+" minutes). \n\nProcess began of enriching data with emails - results will be sent to you at "+email_address_results);
-        console.log("**DONE", li_extractor_data);
-        console.log(JSON.stringify(li_extractor_data));
-        chrome.runtime.sendMessage({"badgetext": ""});
-        total_pages = 0;
-
-        var post_data = {};
-        post_data['send_to_email'] = email_address_results;
-        post_data['data'] = JSON.stringify(li_extractor_data);
-        li_extractor_data = [];
-        $.ajax({
-            type: 'POST',
-            url: liextractor_api_url+'/new_job',
-            data: post_data,
-            dataType: 'json',
-            success: function (data) {
-                console.log("sent to li-extractor server success", data);
-                chrome.runtime.sendMessage({"badgetext": ""});
-            },
-            error: function(e, type, message) {
-                console.log("ERROR", e, type, message);
-                chrome.runtime.sendMessage({"badgetext": ""});
-            }
-        });
-
-        //JSONToCSVConvertor(li_extractor_data, "LinkedInExtractor.csv", true);
-    }
-}
-
-function extract_data() {
-    console.log("Time to extract 'dat data!");
-
-    if (jQuery('.search-results-container').find('#results-list').length) {
-        //LinkedIn Sales Navigator
-        jQuery('.search-results-container').find('#results-list').find('.result').each(function(){
-            var person = {};
-            person.name = jQuery(this).find('.name').text().trim();
-            person.company_name = jQuery(this).find('.company-name').text().trim();
-            person.title = jQuery(jQuery(this).find('.info').find('.info-value')[0]).text().trim();
-            person.location = jQuery(jQuery(this).find('.info').find('.info-value')[2]).text().trim();
-
-            var new_person = {
-              "name": person.name,
-              "location": person.location,
-              "title": person.title,
-              "company_name": person.company_name
-            };
-            li_extractor_data.push(new_person);
-        });
-    }
-    else {
-        //Normal LinkedIn
-        jQuery('.search-result__wrapper').each(function(){
-            var person = {};
-            person.name = jQuery(this).find('.actor-name').text().trim();
-            person.image = jQuery(this).find('.search-result__image').find('img').attr('src') || "";
-            person.location = jQuery(this).find('.subline-level-2').text().trim();
-            person.tagline = jQuery(this).find('.subline-level-1').text().trim();
-            person.current_position = jQuery(this).find('.search-result__snippets').text().replace('Current:', '').trim();
-            person.distance = jQuery(this).find('.dist-value').text().trim();
-            person.title = "";
-            person.company_name = "";
-            if (person.current_position.length > 0) {
-                var comp_pieces = person.current_position.split(" at ");
-                person.title = comp_pieces.shift(); //Drop title
-                person.company_name = comp_pieces.join(" at ");
-            }
-            else {
-                var comp_pieces = person.tagline.split(" at ");
-                person.title = comp_pieces.shift(); //Drop title
-                person.company_name = comp_pieces.join(" at ");
-            }
-            var new_person = {
-              "name": person.name,
-              "location": person.location,
-              "title": person.title,
-              "company_name": person.company_name
-            };
-            li_extractor_data.push(new_person);
-        });
-    }
-    var total_string = li_extractor_data.length;
-    if (total_string > 1000) {
-        total_string = round(total_string/1000, 1).toString()+"k";
-    }
-    else {
-      total_string = total_string.toString();
-    }
-    chrome.runtime.sendMessage({"badgetext": total_string});
-    total_pages += 1;
-
-    next_page();
-}
-
-/*
 $( document ).ready(function() {
-    console.log( "page loaded..." );
-    setInterval(function(){
-        //search-results-container neptune-grid two-column search-is-loading
-        console.log("*****", jQuery('.search-is-loading.search-results-container').length);
-    }, 1000);
+    scan_for_profile();
 });
-*/
-
-function name() {
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
-    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
-    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
-
-    var CSV = '';
-    //Set Report title in first row or line
-
-    CSV += ReportTitle + '\r\n\n';
-
-    //This condition will generate the Label/Header
-    if (ShowLabel) {
-        var row = "";
-
-        //This loop will extract the label from 1st index of on array
-        for (var index in arrData[0]) {
-
-            //Now convert each value to string and comma-seprated
-            row += index + ',';
-        }
-
-        row = row.slice(0, -1);
-
-        //append Label row with line break
-        CSV += row + '\r\n';
-    }
-
-    //1st loop is to extract each row
-    for (var i = 0; i < arrData.length; i++) {
-        var row = "";
-
-        //2nd loop will extract each column and convert it in string comma-seprated
-        for (var index in arrData[i]) {
-            row += '"' + arrData[i][index] + '",';
-        }
-
-        row.slice(0, row.length - 1);
-
-        //add a line break after each row
-        CSV += row + '\r\n';
-    }
-
-    if (CSV == '') {
-        alert("Invalid data");
-        return;
-    }
-
-    //Generate a file name
-    var fileName = "MyReport_";
-    //this will remove the blank-spaces from the title and replace it with an underscore
-    fileName += ReportTitle.replace(/ /g,"_");
-
-    //Initialize file format you want csv or xls
-    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
-
-    // Now the little tricky part.
-    // you can use either>> window.open(uri);
-    // but this will not work in some browsers
-    // or you will not get the correct file extension
-
-    //this trick will generate a temp <a /> tag
-    var link = document.createElement("a");
-    link.href = uri;
-
-    //set the visibility hidden so it will not effect on your web-layout
-    link.style = "visibility:hidden";
-    link.download = fileName + ".csv";
-
-    //this part will append the anchor tag and remove it after automatic click
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
